1cd9fe3ea2effe08c9c05ad7fff92e30e40d5205
nabilaaa29 <nabiladka7@gmail.com>
2024-02-17 19:07:23 +0700

    pawtastic

--- js/script.js
+++ js/script.js
@@ -13,3 +13,23 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+// initialize swiper
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 1,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor : 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+});
